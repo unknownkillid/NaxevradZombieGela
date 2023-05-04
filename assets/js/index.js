d17932a985ca75529f5ebb3ab39c1a6a92892837
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2023-05-04 18:21:27 +0400

    first dialogue done

--- assets/js/index.js
+++ assets/js/index.js
@@ -217,6 +217,7 @@ class Desk {
     }
 }
 
+
 class Cafe {
     constructor({ x, y, cafe, width, height }) {
         this.position = {
@@ -345,8 +346,29 @@ class Impala {
     }
 }
 
+class Snoop {
+    constructor({x, y, snoop, width, height}) {
+        this.position = {
+            x,
+            y
+        }
+
+
+        this.width = width
+        this.height = height
+        this.snoop = snoop
+    }
+
+    draw() {
+        ctx.drawImage(this.snoop, this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 
 let angle = 2
+
+const snoop = new Image()
+snoop.src = './assets/snoopy.png'
 
 const door = new Image()
 door.src = ''
@@ -379,11 +401,16 @@ const desk = new Image()
 desk.src = ''
 
 const player = new Player()
-const welcomeText = ''
+const welcomeText = 'პლანეტა მთლიანად შეიცვალა... ადამიანები თითქმის აღარ დარჩნენ'
+const explain = "უნდა დამეხმარო ამ ყველაფრის ამოხსნაში"
+const dontDie = "უბრალოდ ეცადე გადარჩე..."
 const cafeText = 'უზის დუქანი'
 
 
-
+//snoop
+const snoops = [
+    new Snoop({x: 5600, y: 6000, snoop, width: 110, height: 150})
+]
 //impala 
 const impalas = [
     new Impala({ x: 5300, y: 6000, impala, width: 300, height: 150 })
@@ -417,7 +444,11 @@ const cafes = [new Cafe({ x: 4200, y: window.innerHeight - 596, cafe, width: 800
 const taverns = [new Tavern({ x: 4200, y: window.innerHeight + 6000, tavern, width: 800, height: 500 })]
 
 //desks
-const desks = [new Desk({ x: 600, y: window.innerHeight - 196, desk, width: 306, height: 100, text: welcomeText })]
+const desks = [
+    new Desk({ x: 700, y: window.innerHeight - 196, desk, width: 306, height: 100, text: welcomeText }),
+    new Desk({ x: 1700, y: window.innerHeight - 196, desk, width: 306, height: 100, text: explain }),
+    new Desk({ x: 2650, y: window.innerHeight - 196, desk, width: 306, height: 100, text: dontDie }),
+]
 
 // trees
 const trees = [
@@ -500,12 +531,20 @@ function updatePlatform(platform, { x, y, width, height }) {
     platform.height = height
 }
 
+function updateSnoop(snoop, { x, y, width, height }) {
+    snoop.position.x = x
+    snoop.positiony = y
+    snoop.width = width
+    snoop.height = height
+}
+
 let lastFrameTime = 0;
 let fps = 90;
 
 function animate(currentTime) { 
     
     requestAnimationFrame(animate);
+  
 
     const elapsed = currentTime - lastFrameTime;
 
@@ -530,6 +569,8 @@ function animate(currentTime) {
             trap.draw();
         })
 
+        
+       
         impalas.forEach(impala => {
             impala.draw();
         })
@@ -545,6 +586,7 @@ function animate(currentTime) {
         signs.forEach(sign => {
             sign.draw();
         })
+      
         spinners.forEach(spinnerSaw => {
             if (player.position.x < spinnerSaw.position.x + spinnerSaw.width &&
                 player.position.x + player.width - 10 > spinnerSaw.position.x &&
@@ -565,18 +607,24 @@ function animate(currentTime) {
             platform.draw();
         })
 
+        snoops.forEach(snoop => {
+            snoop.draw();
+        })
+
         bushes.forEach(bush => {
             bush.draw();
         })
         taverns.forEach(tavern => {
             tavern.draw();
         })
+        
         const myTavern = taverns[0]
         const tavern = cafes[0]
         const platformWidth = platforms[0]
         const doorTavern = doors[0]
         const impalaPosition = impalas[0]
         const platform8 = platforms[8]
+        const snoopDogg = snoops[0]
 
         const transition = document.getElementById('transition')
 
@@ -599,7 +647,7 @@ function animate(currentTime) {
                         doorTavern.position.y = window.innerHeight + 1000;
                         updateImpala(impalaPosition, { x: impalaPosition.position.x, y: window.innerHeight - platform8.height - 147, width: 300, height: 150})
                         updatePlatform(platform8, { x: platform8.position.x, y: window.innerHeight - player.height + 51, width: 700, height: 100})
-                        console.log(player.position.y + "player", npc.position.y + "npc")
+                        snoopDogg.position.y = window.innerHeight - 235;
                         npc.position.y = window.innerHeight - 280;
                         offset = 0;
                         player.velocity.x = 0;
@@ -616,6 +664,9 @@ function animate(currentTime) {
 
             door.draw();
 
+            
+          
+
         })
         if (keys.right.pressed && player.position.x < 50) {
             player.velocity.x = 0;
@@ -627,6 +678,11 @@ function animate(currentTime) {
 
         if (keys.right.pressed && offset < 3000) {
             offset += 5;
+
+            //snoop move
+            snoops.forEach(snoop => {
+                snoop.position.x -= 7
+            })
             //impala move 
             impalas.forEach(impala => {
                 impala.position.x -= 7;
@@ -713,6 +769,9 @@ function animate(currentTime) {
             })
             impalas.forEach(impala => {
                 impala.position.x += 7;
+            })
+            snoops.forEach(snoop => {
+                snoop.position.x += 7
             })
         }
 
@@ -884,7 +943,7 @@ testing();
 const pistolPrice = 'ფასი: 200 გელა'
 const priceless = 'არიყიდება უნდა დაიმსახურო!'
 const priceDefault = 'ფასი:'
-const akPrice = 'ფასი: 2000 გელა'
+const akPrice = 'ფასი: 1500 გელა'
 
 const pistolCard = document.getElementById('pistol') 
 const price = document.getElementById('price')
@@ -910,4 +969,125 @@ cards.forEach(card => {
     card.addEventListener('mouseleave', () => {
         price.textContent = priceDefault
     })
+})
+
+
+const continueButton = document.getElementById('epilogueButton')
+const whereIm = document.getElementById('starting')
+let starting = false;
+continueButton.addEventListener('click', () => {
+    whereIm.classList.add('opac0')
+    setTimeout(() => {
+        whereIm.style.display = 'none'
+        starting = true;
+    }, 600);
+})
+
+function started() {
+    if (!starting) {
+        addEventListener('keydown', ({keyCode}) => {
+            switch (keyCode) {
+                case 65:
+                keys.left.pressed = false;
+                break;
+
+                case 68: 
+                keys.right.pressed = false;
+                break;
+            }
+        })
+    } else if (starting) {
+        addEventListener('keydown', ({keyCode}) => {
+            switch (keyCode) {
+                case 65:
+                keys.left.pressed = true;
+                break;
+
+                case 68: 
+                keys.right.pressed = true;
+                break;
+            }
+        })
+    }
+    requestAnimationFrame(started)
+}
+
+started();
+
+const answerBtn1 = document.getElementById('answer')
+const answerBtn2 = document.getElementById('answer2btn')
+const answerBtn3 = document.getElementById('answer3btn')
+const answerBtn4 = document.getElementById('answer4btn')
+const answerBtn5 = document.getElementById('answer5btn')
+const answerBtn6 = document.getElementById('answer6btn')
+const answerBtn7 = document.getElementById('answer7btn')
+const answerBtn8 = document.getElementById('answer8btn')
+const answerBtn9 = document.getElementById('answer9btn')
+const answerBtn10 = document.getElementById('answer10btn')
+const answerBtn11 = document.getElementById('answer11btn')
+const answerBtn12 = document.getElementById('answer12btn')
+const answerBtn13 = document.getElementById('answer13btn')
+
+const dialogue1 = document.getElementById('dialogue1')
+const dialogue2 = document.getElementById('dialogue2')
+const dialogue3 = document.getElementById('dialogue3')
+const dialogue4 = document.getElementById('dialogue4')
+const dialogue5 = document.getElementById('dialogue5')
+const dialogue6 = document.getElementById('dialogue6')
+const dialogue7 = document.getElementById('dialogue7')
+const dialogue8 = document.getElementById('dialogue8')
+const dialogue9 = document.getElementById('dialogue9')
+const dialogue10 = document.getElementById('dialogue10')
+const dialogue11 = document.getElementById('dialogue11')
+const dialogue12 = document.getElementById('dialogue12')
+const dialogue13 = document.getElementById('dialogue13')
+
+answerBtn1.addEventListener('click', () => {
+    dialogue1.classList.add('displaynone')
+    dialogue2.classList.add('displayflex')
+})
+answerBtn2.addEventListener('click', () => {
+    dialogue2.classList.remove('displayflex')
+    dialogue3.classList.add('displayflex')
+})
+answerBtn3.addEventListener('click', () => {
+    dialogue3.classList.remove('displayflex')
+    dialogue4.classList.add('displayflex')
+})
+answerBtn4.addEventListener('click', () => {
+    dialogue4.classList.remove('displayflex')
+    dialogue5.classList.add('displayflex')
+})
+answerBtn5.addEventListener('click', () => {
+    dialogue5.classList.remove('displayflex')
+    dialogue6.classList.add('displayflex')
+})
+answerBtn6.addEventListener('click', () => {
+    dialogue6.classList.remove('displayflex')
+    dialogue7.classList.add('displayflex')
+})
+answerBtn7.addEventListener('click', () => {
+    dialogue7.classList.remove('displayflex')
+    dialogue8.classList.add('displayflex')
+})
+answerBtn8.addEventListener('click', () => {
+    dialogue8.classList.remove('displayflex')
+    dialogue9.classList.add('displayflex')
+})
+answerBtn9.addEventListener('click', () => {
+    dialogue9.classList.remove('displayflex')
+    dialogue10.classList.add('displayflex')
+})
+answerBtn10.addEventListener('click', () => {
+    dialogue10.classList.remove('displayflex')
+    dialogue11.classList.add('displayflex')
+})
+answerBtn11.addEventListener('click', () => {
+    dialogue11.classList.remove('displayflex')
+    dialogue12.classList.add('displayflex')
+})
+answerBtn12.addEventListener('click', () => {
+    dialogue12.classList.remove('displayflex')
+    dialogue13.classList.add('displayflex')
+    menu.style.display = 'none';
 })
